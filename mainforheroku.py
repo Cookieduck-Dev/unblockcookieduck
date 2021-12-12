e2bcc1ae1e5b3ee55de4c7fcf5a1635e95e406a6
IsaacLK <50344947+IsaacLK@users.noreply.github.com>
2021-12-11 23:47:57 -0500

    Update mainforheroku.py

--- mainforheroku.py
+++ mainforheroku.py
@@ -1,4 +1,4 @@
 import cdub
 #cdub.config() options: "cookieduck", "betteralgebra", "simplecalculator"
 cdub.config("cookieduck")
-cdub.run(debug = False, host="0.0.0.0",port=8000)
+cdub.run(debug = False, host="0.0.0.0",port=5000)
