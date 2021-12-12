f7a110e5f0e64e453aeb0a125ff7cb685ad61bfa
IsaacLK <50344947+IsaacLK@users.noreply.github.com>
2021-12-11 23:16:32 -0500

    Trying to fix stuff.

--- setup.py
+++ setup.py
@@ -1,7 +1,3 @@
 #setup for heroku deployment
 import os 
-os.system("pip install setuptools")
-os.system("pip install flask")
-os.system("pip install requests")
-os.system("pip install git+https://github.com/Cookieduck-Dev/unblockcookieduck")
 os.system("python main.py")
