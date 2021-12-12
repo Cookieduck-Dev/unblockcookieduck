93720aafc60ab8fdbe7dd6166d315909322eb320
IsaacLK <50344947+IsaacLK@users.noreply.github.com>
2021-12-11 23:13:09 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #setup for heroku deployment
 import os 
+os.system("pip install setuptools")
 os.system("pip install git+https://github.com/Cookieduck-Dev/unblockcookieduck")
 os.system("python main.py")
